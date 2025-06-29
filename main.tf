3364ea121ece8c4e6a161458d8a1fec38a41371c
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-06-29 11:59:05 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = "1.10.4"
-
+  required_version = ">= 1.10.4"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.84.0"
     }
-
     random = {
       source  = "hashicorp/random"
       version = "3.6.3"
